3bc93b0c05d8cba36abb5fe1384261eabc823178
Scott Fox <scottfox87@gmail.com>
2018-09-12 18:31:20 -0500

    set up grid component

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Grid.css';
+
+const grid = (props) => (
+    <div className={classes.Grid}>
+        Grid
+    </div>
+);
+
+
+export default grid;
